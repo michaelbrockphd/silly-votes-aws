994112b8e14535df085fc30b5c4026c3e4e4cc31
Michael Brock <michael.brock@dfaw.com.au>
2021-08-12 14:17:28 +1000

    Small bit of code cleanup.  Also helped having the following sources to solve my remaining cognito issues:
    * https://github.com/aws-amplify/amplify-js/issues/3283
        * This reminded me I still had a cookie configured for debugging and never removed it.
    
    * https://aws.amazon.com/premiumsupport/knowledge-center/elasticsearch-authentication-kibana/
        * Setting the require IAM role so I didn't get those annoying POST 400 errors!
    
    Also updated the login route so it properly returns to the profile page once the user is logged in (not happy with the render() kludge though).

--- src/amplify-utils/index.mjs
+++ src/amplify-utils/index.mjs
@@ -5,24 +5,7 @@ const createAuthOptions = () => {
         region: environment.REACT_APP_AMP_COG_REGION,
         userPoolId: environment.REACT_APP_AMP_COG_ID_USER_POOL,
         userPoolWebClientId: environment.REACT_APP_AMP_COG_ID_APP_CLIENT,
-        identityPoolId: environment.REACT_APP_AMP_COG_ID_IDENTITY_POOL,
-
-        /*oauth: {
-            domain: environment.REACT_APP_AMP_COG_DOMAIN,
-            scope: [ "email" ],
-            redirectSignIn: environment.REACT_APP_AMP_COG_URL_POST_SIGN_IN,
-            redirectSignOut: environment.REACT_APP_AMP_COG_URL_POST_SIGN_OUT,
-            responseType: "code"  // code or token
-        },
-
-        federationTarget: "COGNITO_USER_POOLS",
-
-        cookieStorage: {
-            domain: 'localhost',
-            secure: false,
-            path: '/',
-            expires: 365,
-          }*/
+        identityPoolId: environment.REACT_APP_AMP_COG_ID_IDENTITY_POOL
     };
     
     return(rtn);
